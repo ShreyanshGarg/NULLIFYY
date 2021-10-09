7a567f78e35d760f21f74ddddd10e1a2ce7cee6a
Tanmay Jain <jain2015tanmay@gmail.com>
2021-10-09 22:21:48 +0530

    Add imput to modal

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.post("/main", function(req, res) {
 //   console.log("Button clicked");
 // });
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.listen(3000, function() {
   console.log("Server Started on 3000");
--- public/main.js
+++ public/main.js
@@ -1,3 +1,16 @@
-function printDiv(divName) {
-console.log("hi");
+function expense(divName) {
+  var modalExp = document.getElementById("exp-modal");
+  modalExp.style.display = "block";
+}
+
+function closeExp() {
+  var modalExp = document.getElementById("exp-modal");
+  modalExp.style.display = "none";
+}
+
+window.onclick = function(event) {
+  var modalExp = document.getElementById("exp-modal");
+  if (event.target == modalExp) {
+    modalExp.style.display = "none";
+  }
 }
