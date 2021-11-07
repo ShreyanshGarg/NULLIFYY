b94d12c6e3dbe692dff82626fcea138e3630ceb4
Shreyansh Garg <guptashreyansh22@gmail.com>
2021-11-07 13:01:29 +0530

    frnd list display in expense modal

--- public/main.js
+++ public/main.js
@@ -3,15 +3,43 @@ function expense() {
   modalFriends.style.display = "block";
 }
 
+
+let list = [];
+
 function addFriend(divName) {
-  var modalExp = document.getElementById("exp-modal");
-  var modalFriends = document.getElementById("friends-modal");
-  modalExp.style.display = "block";
-  modalFriends.style.display = "none";
+
+  // code for retrieving values from of selected friend
+  var checkboxes = document.getElementsByName('friends_list');
+  for (var checkbox of checkboxes) {
+    if (checkbox.checked) {
+      // inserting friends in an array for further use
+      list.push(checkbox.value);
+      // console.log(checkbox.value);
+    }
+  }
+  // displaying warning msg if frnd_list is empty
+  if (list.length === 0) {
+    document.getElementById("warning_frnd").style.display = "block";
+  } else {
+    var modalExp = document.getElementById("exp-modal");
+    var modalFriends = document.getElementById("friends-modal");
+    modalExp.style.display = "block";
+    modalFriends.style.display = "none";
+    var ll = list.length;
+    ll = ll - 1;
+    if (ll >= 1) {
+      document.getElementById("frnd_names").value = list[0] + " and " + ll + " more ";
+    } else {
+      document.getElementById("frnd_names").value = list[0];
+    }
+  }
 }
+
 
 function closeExp() {
   var modalExp = document.getElementById("exp-modal");
+  list = [];
+  closeFriend();
   modalExp.style.display = "none";
 }
 
@@ -25,5 +53,15 @@ window.onclick = function(event) {
 
 function closeFriend() {
   var modalFriends = document.getElementById("friends-modal");
+  // turning warning message off
+  document.getElementById("warning_frnd").style.display = "none";
+  // unchecking selected friends after closing the modal
+  var checkboxes = document.getElementsByName('friends_list');
+  for (var checkbox of checkboxes) {
+    if (checkbox.checked) {
+      checkbox.checked = false;
+    }
+  }
+  // close modal
   modalFriends.style.display = "none";
 }
