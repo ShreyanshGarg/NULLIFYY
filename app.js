5f023ce934e8a21622a2594794b0df064250aa12
Shreyansh Garg <guptashreyansh22@gmail.com>
2021-11-10 14:43:04 +0530

    bad request error

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const ejs = require("ejs");
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const app = express();
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/profileDB", {
   useNewUrlParser: true,
@@ -13,23 +30,24 @@ mongoose.connect("mongodb://localhost:27017/profileDB", {
 
 var msg = 0;
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  email: String,
+  username: String,
   password: String,
   phoneno: Number,
   friends: [String]
-};
+});
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('User', userSchema);
 
+passport.use(User.createStrategy());
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 app.get("/signup", function(req, res) {
@@ -64,35 +82,50 @@ app.post("/dashboard", function(req, res) {
 
   if (req.body.button == "signup_page") {
 
-User.findOne({email:req.body.emailId},function(err,foundduplicate){
-  if(err){
-    console.log(err);
-  }
-  else{
-    //  If email id Already Exist
-    if(foundduplicate)
-    {
-      msg=1;
-      res.redirect("/signup")
-    }
-    else{
-      const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.emailId,
-        password: req.body.password,
-        phoneno: req.body.phoneNo,
-        friends: ["Titu","Golu"]
-      })
-      var friends =  ["Titu","Golu"];
-      user.save();
-      res.render("main", {
-        firstname: req.body.firstName,
-        lastname: req.body.lastName,
-        friendList: friends
-      });
-    }
-  }
+// User.findOne({email:req.body.emailId},function(err,foundduplicate){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     //  If email id Already Exist
+//     if(foundduplicate)
+//     {
+//       msg=1;
+//       res.redirect("/signup")
+//     }
+//     else{
+//       const user = new User({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         email: req.body.emailId,
+//         password: req.body.password,
+//         phoneno: req.body.phoneNo,
+//         friends: ["Titu","Golu"]
+//       })
+//       var friends =  ["Titu","Golu"];
+//       user.save();
+//       res.render("main", {
+//         firstname: req.body.firstName,
+//         lastname: req.body.lastName,
+//         friendList: friends
+//       });
+//     }
+//   }
+// })
+
+User.register({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          username: req.body.emailId,
+          phoneno: req.body.phoneNo,
+          friends: []
+},req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+    }console.log(user);        
+    passport.authenticate("local")(req, res, function(){
+            res.redirect("/");
+        });
 })
 
   }
@@ -134,6 +167,22 @@ app.post("/main", function(req, res) {
 
 app.post("/temp", function(req, res) {
   console.log(req.body.friends);
+  res.render("main", {
+    firstname: "shreyansh",
+    lastname: "garg",
+    friendList: ["titu","golu"]
+  });
+})
+app.post("/invite", function(req, res) {
+  console.log(req.body.invited_frnd);
+
+User.findOne({email:req.body.invited_frnd},function(err,result){
+  if(err){console.log(err)}
+  else{
+
+  }
+})
+
   res.render("main", {
     firstname: "shreyansh",
     lastname: "garg",
