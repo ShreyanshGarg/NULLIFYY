eee061a230037ed6ea0c2ce1425dd7dd55733694
ih1802imran <ih1802imran@gmail.com>
2022-07-14 22:25:26 +0530

    Error removed of dashboard rerouting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const session = require("express-session");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
